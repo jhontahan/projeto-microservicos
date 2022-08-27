397829f02d0abb6a82ed447b4ad914cafb687a9f
jhonathan Cruz <jhonathancruz48@gmail.com>
2022-08-27 14:21:36 -0300

    criando o microservicos de folha de pagmento

---
+++ hr-folhapagamento/src/main/java/com/cursospring/folhapagamento/HrFolhapagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.folhapagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrFolhapagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrFolhapagamentoApplication.class, args);
+	}
+
+}
---
+++ hr-folhapagamento/src/test/java/com/cursospring/folhapagamento/HrFolhapagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.folhapagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrFolhapagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
